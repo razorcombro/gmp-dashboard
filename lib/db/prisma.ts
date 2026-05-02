30eebc5fd07bab7f136399f1c2f20aabeaabad4b
razorcombro <saepulhayat22@gmail.com>
2026-05-03 00:59:36 +0700

    feat: initial dashboard with prisma data

--- lib/db/prisma.ts
+++ lib/db/prisma.ts
@@ -2,7 +2,6 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 declare global {
-  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
@@ -14,7 +13,6 @@ export const prisma =
   global.prisma ||
   new PrismaClient({
     adapter,
-    log: ["error", "warn"],
   });
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
